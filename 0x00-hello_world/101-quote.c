b6b56fc6c983edda8a0a12faa638aa1538ea327e
sam <samueladeleke302@gmail.com>
2022-10-13 17:03:17 -0700

    ke

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
+
 #include <unistd.h>
 /**
- * main-program entry point.
+ *main-program entry point.
  *
- * Return: 1 on success.
+ *Return: 1 on success.
  */
 int main(void)
 {
-	write (2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59 char long);
-	return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
