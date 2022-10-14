3370c738e336ed873e93fc017f38487ffef51027
sam <samueladeleke302@gmail.com>
2022-10-14 14:03:57 -0700

    3-4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,23 +3,17 @@
  * main - entry point
  * Return: 0
  */
+
 int main(void)
 {
 	char c;
-	char d;
-
 	c = 'a';
-	d = 'A';
 	while
 		(c <= 'z') {
-			putchar(c);
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
 			c++;
-		}
-	while
-		(d <= 'Z') {
-			putchar(d);
-			d++;
 		}
 	putchar('\n');
 	return (0);
-}
+}	
