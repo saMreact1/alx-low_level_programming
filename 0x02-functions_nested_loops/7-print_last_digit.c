4c2209909875db0bebf0acd03dc558de7ddba550
sam <samueladeleke302@gmail.com>
2022-10-18 16:06:39 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,11 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int x;
 
-	if (ld < 0)
-		ld = -1;
-
-	_putchar(ld + '0');
-
-	return (0);
+	if (n < 0)
+		n = -n;
+	x = n % 10;
+	_putchar(x + '0');
+	return (x);
 }
