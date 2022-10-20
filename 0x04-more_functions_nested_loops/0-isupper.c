0c264076b9a554875981c6e9325054eecf196bef
sam <samueladeleke302@gmail.com>
2022-10-20 09:12:35 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - checks for uppercase character
  * @c: the character to be checked
- * Return: 1 if c is uppercase, 0 if otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN.H
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,8 +10,8 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+int main(void);
 void print_triangle(int size);
+int main(void);
 void print_number(int n);
-int main(void)
 
-#endif
